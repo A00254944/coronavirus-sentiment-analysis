d1121daa81d4a27e13952fb2a683622934bf046a
Craig Cunningham <64531211+A00254944@users.noreply.github.com>
2021-05-24 13:37:52 +0100

    Update covid_stats.js

--- covid_stats.js
+++ covid_stats.js
@@ -2,7 +2,7 @@
 fetch("https://covid-19-statistics.p.rapidapi.com/reports?date=2020-04-16", {
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-key": "70a1417856msh303faf5b26b3b15p1a4004jsn7a7f7cd83db0",
+		"x-rapidapi-key": "",
 		"x-rapidapi-host": "covid-19-statistics.p.rapidapi.com"
 	}
 })
